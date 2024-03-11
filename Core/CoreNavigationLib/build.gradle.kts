7721090cd02d6d28860da4382a99516b5bc2c91c
chekunkov.a.v <coder-chekunkov@yandex.ru>
2024-03-11 23:16:56 +0300

    MPA-019: произвел каторизацию модулей ядра.

--- Core/CoreNavigationLib/build.gradle.kts
+++ Core/CoreNavigationLib/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":CoreLogsLib"))
+    implementation(project(":Core:CoreLogsLib"))
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
